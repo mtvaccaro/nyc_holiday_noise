8daafc917d33d5558ab8ec92997dd5c42542b654
Matt Vaccaro <matt.t.vaccaro@gmail.com>
2015-11-16 12:43:09 -0500

    new ajax structure
    restructured ajax call with .success function and a subsequent wait function to try to compensate for asynchronous call time

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+//setting up map and tileLayer
 var map = L.map('map').setView([40.7518685, -73.984857], 11);
     
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
@@ -6,42 +7,31 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	maxZoom: 19})
     .addTo(map);
 
-
-
+//gets dataset and formats it in json
 var lmarker;
 var laborDayData = "https://data.cityofnewyork.us/resource/m2gt-6ver.json?$$app_token=EANt8JqkhUJOacXDyShkPtTSx";
 
 function ajax() {
-    return $.ajax({
+    $.ajax({
         url: laborDayData,
-        dataType: "json"
-    });
-}
-
-function displayData(x){
-    x.success(function(data){
-        for (var i = 0; i < data.length; i++){
-        lmarker = L.circleMarker(new L.LatLng(data[i].latitude, data[i].longitude))
-            .setRadius(4)
-            .addTo(map)
-            .bindPopup(data[i].created_date);
+        error: function(){
+            console.log("An error has occurred");
+        },
+        dataType: 'json',
+        success: function(data){
+            for (var i=0; i < data.length; i++){
+                lmarker = L.circleMarker(new L.LatLng(data[i].latitude, data[i].longitude))
+                    .setRadius(4)
+                    .addTo(map)
+                    .bindPopup(data[i].created_date);
+            }
         }
     });
 }
 
-var promise = ajax();
-displayData(promise);
-
-//console.log(lmarker);
-
-laborLayer = L.tileLayer(lmarker);
-//halloweenLayer = L.tileLayer(hmarker);
-
-var overlay = {
-    //"Halloween": halloweenLayer,
-    "Labor Day": laborLayer
-};
-
-L.control.layers(overlay).addTo(map);
-
-//console.log(halloweenData);
+$.when(ajax()).done(function layerControl(lmarker) {
+    var overlay = {
+        "Labor Day": L.tileLayer(lmarker)
+    };
+    L.control.layers(overlay).addTo(map);
+});
